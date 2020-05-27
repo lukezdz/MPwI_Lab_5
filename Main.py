93e97bd9c7bc23b2792b499e2b195c653ecc0d34
lukezdz <lukas.zdziarski@gmail.com>
2020-05-28 00:04:24 +0200

    Make CLI more readable

--- Main.py
+++ Main.py
@@ -9,20 +9,24 @@ if __name__ == "__main__":
 
 	for line in lines:
 		numbers.append(float(line.rstrip().replace(",", ".")))
-	
+
+	print()	
 	print("Average: ")
 	average = Calculator.average(numbers)
 	print(average)
 
+	print()
 	print("Weighted average:")
 	weightedAverage = Calculator.weightedAverage(numbers)
 	print(weightedAverage)
 
+	print()
 	print("Normal moment:")
 	normalMomentOne = Calculator.normalMoment(numbers, 1)
 	normalMomentTwo = Calculator.normalMoment(numbers, 2)
 	print('First: {0}, Second: {1}'.format(normalMomentOne, normalMomentTwo))
 
+	print()
 	print("Central moment:")
 	centralMomentOne = Calculator.centralMoment(numbers, 1, average)
 	centralMomentTwo = Calculator.centralMoment(numbers, 2, average)
