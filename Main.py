e3a1039977225db540234d3aa900527e526427c6
lukezdz <lukas.zdziarski@gmail.com>
2020-05-27 23:08:44 +0200

    Load file and show results

---
+++ Main.py
@@ -0,0 +1,29 @@
+from Calculator import Calculator
+
+if __name__ == "__main__":
+	filename = "100liczb_modified.txt"
+
+	numbersFile = open(filename)
+	lines = numbersFile.readlines()
+	numbers = []
+
+	for line in lines:
+		numbers.append(float(line.rstrip().replace(",", ".")))
+	
+	print("Average: ")
+	average = Calculator.average(numbers)
+	print(average)
+
+	print("Weighted average:")
+	weightedAverage = Calculator.weightedAverage(numbers)
+	print(weightedAverage)
+
+	print("Normal moment:")
+	normalMomentOne = Calculator.normalMoment(numbers, 1)
+	normalMomentTwo = Calculator.normalMoment(numbers, 2)
+	print('First: {0}, Second: {1}'.format(normalMomentOne, normalMomentTwo))
+
+	print("Central moment:")
+	centralMomentOne = Calculator.centralMoment(numbers, 1, average)
+	centralMomentTwo = Calculator.centralMoment(numbers, 2, average)
+	print('First: {0}, Second: {1}'.format(centralMomentOne, centralMomentTwo))
